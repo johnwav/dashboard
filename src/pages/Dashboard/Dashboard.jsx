577060c0184b1ff89700182274bb0eab6550d064
johnwav <102820657+johnwav@users.noreply.github.com>
2023-03-23 23:47:22 +0100

    Orders Componet Implemented

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,3 +1,6 @@
+import Orders from "../../components/Orders/Orders";
+import Statistics from "../../components/Statistics/Statistics";
+import { cardsData, groupNumber } from "../../data";
 import css from "./Dashboard.module.css";
 
 const Dashboard = () => {
@@ -16,11 +19,31 @@ const Dashboard = () => {
               </select>
             </div>
           </div>
-          <div className={css.cards}>cards</div>
+          <div className={css.cards}>
+            {cardsData.map((card, index) => (
+              <div className={css.card}>
+                <div className={css.cardHead}>
+                  <span>{card.title}</span>
+                  <span>{card.change}</span>
+                </div>
+
+                <div className={css.cardAmount}>
+                  <span>$</span>
+                  <span>{groupNumber(card.amount)}</span>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
+
+      
+        <Statistics />
+
       </div>
 
-      <div className={css.orders}>orders</div>
+
+
+        <Orders />
     </div>
   );
 };
