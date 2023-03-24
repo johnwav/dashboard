bbd0b07546f1dfa20fedfad6ad3bd5738176f670
johnwav <102820657+johnwav@users.noreply.github.com>
2023-03-24 18:37:51 +0100

    Board page implementation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Calendar from "./pages/Calendar/Calendar";
+import BoardPage from "./pages/Board/Board";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
 
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="calendar" element={<Calendar />} />
+            <Route path="board" element={<BoardPage />} />
 
           </Route>
 
